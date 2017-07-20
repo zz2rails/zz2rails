ab43907bc2b8d84fd7b50d02e10ee7648e2a6ed0
Mahesh Kumar <zz2rails@gmail.com>
2017-07-21 04:46:49 +0530

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/welcome'
+
+  root 'home#welcome'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
